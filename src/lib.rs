0a1a33ceb9dabd7d62ff8edfa38e18ef51f51563
Sergey Shandar <sergey.shandar@gmail.com>
2018-05-04 10:00:44 -0700

    Dynamic buffer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+extern crate num_traits;
+
+type Result<T> = [T; 8];
+
+type Input<T> = [T; 16];
+
+struct State<T> {
+    result: Result<T>,
+    w: Vec<T>
+}
+
+impl<T> State<T>
+    where T : num_traits::PrimInt
+{
+    fn new(w_size: usize, initial: &Result<T>) -> Self {
+        Self {
+            result: *initial,
+            w: vec![T::zero(); w_size]
+        }
+    }
+
+    fn add(input: &Input<T>) {
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
